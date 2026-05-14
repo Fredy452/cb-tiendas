e8de9817c875577cd170c5163cff078426b4a447
Fredy Arce <fredyarce452@gmail.com>
2026-05-14 11:21:06 -0300

    feat: agregando nuevos campos  y agregando dentro de sectión para que se vea mas organizado

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\Categories\Schemas;
 
 use App\Models\Category;
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 
 class CategoryForm
 {
@@ -12,21 +15,39 @@ class CategoryForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->label('Nombre')
-                    ->maxLength(255)
-                    ->live(debounce: 500)
-                    ->afterStateUpdated(fn (callable $set, ?string $state) => $set('slug', str()->slug($state)))
-                    ->required(),
-                TextInput::make('slug')
-                    ->label('Slug')
-                    ->maxLength(255)
-                    ->dehydrated(true)
-                    ->unique(Category::class, 'slug', ignoreRecord: true),
-                TextInput::make('description')
-                    ->label('Descripción')
-                    ->maxLength(255)
-                    ->required(),
+                    Section::make('Categoria')
+                    ->description('Información de la categoría')
+                    ->icon('heroicon-o-tag')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nombre')
+                            ->live(debounce: 500)
+                            ->afterStateUpdated(
+                                fn (callable $set, ?string $state) =>
+                                $set('slug', str()->slug($state))
+                            )
+                            ->required(),
+
+                        TextInput::make('slug')
+                            ->label('Slug')
+                            ->dehydrated(true)
+                            ->unique(Category::class, 'slug', ignoreRecord: true),
+
+                        Textarea::make('description')
+                            ->label('Descripción')
+                            ->required(),
+                    ]),
+
+                    Section::make('Info')
+                    ->schema([
+                        Toggle::make('is_active')
+                        ->label('¿Está activa?'),
+
+                        TextInput::make('display_order')
+                            ->label('Orden de visualización')
+                            ->numeric()
+                            ->default(0)
+                    ])
             ]);
     }
 }
