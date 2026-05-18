413611b07cdc8d07fc52be77ba1ef76e481b19b9
Fredy Arce <fredyarce452@gmail.com>
2026-05-18 09:21:53 -0300

    feat: nuevas rutas para vistas de tiendas, categorias, emprendimientos y sobre nosotros

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TiendaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [TiendaController::class, 'home'])->name('home');
 Route::get('/tiendas', [TiendaController::class, 'index'])->name('tiendas.index');
+Route::get('/tiendas/{store}', [TiendaController::class, 'show'])->name('tiendas.show');
 Route::get('/categorias', [TiendaController::class, 'categorias'])->name('categorias');
+Route::get('/emprendimientos/registrar', [TiendaController::class, 'create'])->name('emprendimientos.create');
+Route::post('/emprendimientos/registrar', [TiendaController::class, 'store'])->name('emprendimientos.store');
+Route::get('/sobre-nosotros', [TiendaController::class, 'about'])->name('sobre-nosotros');
