7cd9e3e9ba71bf97dbdaf501cb12ccf8c3b5c200
Fredy Arce <fredyarce452@gmail.com>
2026-06-15 23:45:52 -0300

    feat: agregar nuevos íconos y manifest para soporte de PWA

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -54,6 +54,12 @@
         <meta name="application-name" content="{{ $siteName }}">
         <meta name="theme-color" content="#0f5238">
         <link rel="canonical" href="{{ $canonicalUrl }}">
+        <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-96x96.png') }}" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}" />
+        <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}" />
+        <meta name="apple-mobile-web-app-title" content="Cb-tiendas" />
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
 
         <meta property="og:site_name" content="{{ $siteName }}">
         <meta property="og:locale" content="{{ str_replace('-', '_', app()->getLocale()) }}">
@@ -105,5 +111,6 @@
 
             @include('partials.public.footer')
         </div>
+        @stack('scripts')
     </body>
 </html>
