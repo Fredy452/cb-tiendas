f3b95c51fee1d39a4eafb3b0890c092d754f39d9
Fredy Arce <fredyarce452@gmail.com>
2026-08-09 11:57:47 -0300

    feat: mejora el sitemap para incluir imágenes y títulos de las tiendas

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,11 +1,18 @@
 {!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 @foreach ($urls as $entry)
     <url>
         <loc>{{ $entry['loc'] }}</loc>
         <lastmod>{{ $entry['lastmod'] }}</lastmod>
         <changefreq>{{ $entry['changefreq'] }}</changefreq>
         <priority>{{ $entry['priority'] }}</priority>
+@if (! empty($entry['image']))
+        <image:image>
+            <image:loc>{{ $entry['image'] }}</image:loc>
+            <image:title>{{ $entry['image_title'] }}</image:title>
+        </image:image>
+@endif
     </url>
 @endforeach
 </urlset>
--- routes/web.php
+++ routes/web.php
@@ -42,16 +42,22 @@ Route::get('/sitemap.xml', function () {
 	])
 		->merge(
 			Store::query()
-				->publicVisible()
-				->select(['id', 'slug', 'updated_at'])
-				->latest('updated_at')
-				->get()
-				->map(fn (Store $store) => [
-					'loc' => route('tiendas.show', $store->slug ?: $store->getKey()),
-					'lastmod' => optional($store->updated_at)->toDateString() ?: now()->toDateString(),
-					'changefreq' => 'weekly',
-					'priority' => '0.7',
-				])
+                ->publicVisible()
+				->select(['id', 'name', 'slug', 'updated_at', 'logo_path', 'img_path'])
+                ->latest('updated_at')
+                ->get()
+				->map(function (Store $store) {
+					$image = $store->cover_url ?: $store->logo_url;
+
+					return [
+						'loc' => route('tiendas.show', $store->slug ?: $store->getKey()),
+						'lastmod' => optional($store->updated_at)->toDateString() ?: now()->toDateString(),
+						'changefreq' => 'weekly',
+						'priority' => '0.7',
+						'image' => $image ? url($image) : null,
+						'image_title' => $store->name,
+					];
+				})
 		)
 		->merge(
 			Category::query()
